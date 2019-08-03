ed93b52634a98813536671f1a4a777e87976fd75
Kyle Connolly <webmasterconnolly@gmail.com>
2019-08-02 21:50:44 -0300

    Remove master branch's JS files

--- src/util/configurePlugins.js
+++
@@ -1,9 +0,0 @@
-export default function(pluginConfig, plugins) {
-  const pluginNames = Array.isArray(pluginConfig)
-    ? pluginConfig
-    : Object.keys(plugins).filter(pluginName => {
-        return pluginConfig !== false && pluginConfig[pluginName] !== false
-      })
-
-  return pluginNames.map(pluginName => plugins[pluginName]())
-}
